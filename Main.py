b22cf8ef04e6c056143bd52c63c44a40ffb0ee06
Luka98122 <luka98122@gmail.com>
2022-06-28 12:17:23 +0200

    Large changes
    Camera movement, larger map, more tiles.

--- Main.py
+++ Main.py
@@ -1,34 +1,67 @@
 import pygame
 import sys
 
+cameraOffset = pygame.Vector2(0, 0)
 prozor = pygame.display.set_mode((1000, 1000))
-movementCooldown = 1000
-# X = zid, O = vazduh, C = coin, S = sanduk (chest)
+movementCooldown = 50
+latestMove = pygame.Vector2(0, 0)
+# X = zid, O = vazduh, C = coin, S = sand, W = water
 mapa = [
-    "XXXXXXXXXXO",
-    "XOOOOOOOOXO",
-    "XXOOOOOOOXO",
-    "XOOOOOXXXXO",
-    "XOOOXXXOOXO",
-    "XOOXXXOOOXO",
-    "XOOOOXXXOXO",
-    "XOOOOOOOOXO",
-    "XOOOOOOOOXO",
-    "XXXXXXXXXXO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOSSOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOSSSSSSSSSOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOSSSSSSSOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOSSSSSSOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOSSSOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOSOOOOOOOOOOSSSSSSSSSSSSOOOOOOOOOOSSSOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOSSSOOOOOOOSSSSSSSSSSSSSSOOOOOOOSSSSSSSOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOSOOOOOOOSSWWWWWWWWWWWSSSSOOSSSSWWWWSSOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOSSSOOOOSSSSSSSWWWWWWWWWWSSSSWWWWSSSSSOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOSSSSSSSSSSWWWWWWWWWWWWWWWWWWWWWSSSOOOOOOOOOOO",
+    "OOOOOOOOOOOOXXXXXXXXXXXXOOOOOOSSSWWWWWWWWWWWWWWWWWWWWWWWSSOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOXOOOOOOOSSSSSWWWWWWWWWWWWWWWWWWWSSOOOOOOOOOOOO",
+    "OOOOOOOOOOOOXOOOOOOOOOOXOOOOOOSSSWWWWWWWWWWWWWWWWWWWWWWSSOOOOOOOOOOOO",
+    "OOOOOOOOOOOOXXXXOOOOOOOXOOOOOOOOSSSSSSSWWWWWWWWWWWWWWWSSOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOXOOOOOOOXOOOOOOOOOOOOSSSSSWWWWWWWSSSSSSSOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOXOOOOOOOXOOOOOOOOOOOOOOOSSSSSSSSSSSSSSSOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOXXXXXXXXXOOOOOOOOOOOOOOOOOSSSSSSSSOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+    "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
 ]
 
 
 # Texture
+# ====================LOAD=======================#
 Chest1 = pygame.image.load("Textures\Chest1.png")
 OpenedChest1 = pygame.image.load("Textures\OpenedChest1.png")
 Dirt = pygame.image.load("Textures\Dirt.jpg")
 StoneFloor = pygame.image.load("Textures\StoneFloor.jpg")
 warrior = pygame.image.load("Textures\warrior.png")
+water1 = pygame.image.load("Textures\Water1.png")
+sand = pygame.image.load("Textures\sand.png")
+ClosedDoor = pygame.image.load("Textures\ClosedDoor.png")
+OpenedDoor = pygame.image.load("Textures\OpenedDoor.png")
+# =====================SCALE=====================#
 StoneFloor = pygame.transform.scale(StoneFloor, (100, 100))
 Dirt = pygame.transform.scale(Dirt, (100, 100))
 Chest1 = pygame.transform.scale(Chest1, (100, 100))
 warrior = pygame.transform.scale(warrior, (100, 100))
 OpenedChest1 = pygame.transform.scale(OpenedChest1, (100, 100))
+water1 = pygame.transform.scale(water1, (100, 100))
+sand = pygame.transform.scale(sand, (100, 100))
+ClosedDoor = pygame.transform.scale(ClosedDoor, (100, 100))
+OpenedDoor = pygame.transform.scale(OpenedDoor, (100, 100))
 
 
 def isPassable(
@@ -36,7 +69,7 @@ def isPassable(
     y,
 ):
     # Prvo y pa x, jer prvo nadjemo visinu, i onda idemo kroz red, ovo nije bug
-    if mapa[y][x] == "O":
+    if mapa[y][x] != "X" and mapa[y][x] != "W":
         return True
     else:
         return False
@@ -44,8 +77,7 @@ def isPassable(
 
 ##### Entities (chests, enemies, missiles)
 class Entity:
-    x = 0
-    y = 0
+    pos = pygame.Vector2(0, 0)
     type = 0
     opened = 0
 
@@ -56,9 +88,21 @@ class Entity:
         if self.type == "Chest":
             # print("Did it prior")
             if self.opened == 0:
-                prozor.blit(Chest1, (self.x * 100, self.y * 100))
+                prozor.blit(
+                    Chest1,
+                    (
+                        j * 100 - int(cameraOffset.x) * 100,
+                        i * 100 - int(cameraOffset.y) * 100,
+                    ),
+                )
             if self.opened == 1:
-                prozor.blit(OpenedChest1, (self.x * 100, self.y * 100))
+                prozor.blit(
+                    OpenedChest1,
+                    (
+                        j * 100 - int(cameraOffset.x) * 100,
+                        i * 100 - int(cameraOffset.y) * 100,
+                    ),
+                )
 
 
 entityList = []
@@ -66,8 +110,8 @@ entityList = []
 
 def createNewEntity(x, y, type):
     entity = Entity()
-    entity.x = x
-    entity.y = y
+    entity.pos.x = x
+    entity.pos.y = y
     entity.type = type
     entityList.append(entity)
 
@@ -76,15 +120,17 @@ createNewEntity(8, 1, "Chest")
 
 
 class Player:
-    pos = pygame.Vector2(1, 1)
+    startx = 5
+    starty = 5
+    pos = pygame.Vector2(startx, starty)
     speed = 5
     hp = 0
     movementCooldown = 0
-    defaultCooldown = 50
+    defaultCooldown = 25
 
     def Activations(self):
         for i in range(len(entityList)):
-            if self.pos.x == entityList[i].x and self.pos.y == entityList[i].y:
+            if self.pos.x == entityList[i].pos.x and self.pos.y == entityList[i].pos.y:
                 entityList[i].opened = 1
 
     def Update(self):
@@ -94,11 +140,13 @@ class Player:
 
     def Draw(self):
         # pygame.draw.rect(prozor,pygame.Color("Red"),pygame.Rect(self.pos.x * 100, self.pos.y * 100, 100, 100))
-        prozor.blit(warrior, (self.pos.x * 100, self.pos.y * 100))
+        prozor.blit(warrior, (self.startx * 100, self.starty * 100))
 
     def Move(
         self,
     ):
+        global cameraOffset
+        global latestMove
         if self.movementCooldown < 0:
             keys = pygame.key.get_pressed()
             if (
@@ -107,24 +155,28 @@ class Player:
             ):
                 self.pos.y = self.pos.y - 1
                 self.movementCooldown = self.defaultCooldown
+                cameraOffset.y -= 1
             if (
                 keys[pygame.K_DOWN]
                 and isPassable(int(self.pos.x), int(self.pos.y + 1)) == True
             ):
                 self.pos.y = self.pos.y + 1
                 self.movementCooldown = self.defaultCooldown
+                cameraOffset.y += 1
             if (
                 keys[pygame.K_LEFT]
                 and isPassable(int(self.pos.x - 1), int(self.pos.y)) == True
             ):
                 self.pos.x = self.pos.x - 1
                 self.movementCooldown = self.defaultCooldown
+                cameraOffset.x -= 1
             if (
                 keys[pygame.K_RIGHT]
                 and isPassable(int(self.pos.x + 1), int(self.pos.y)) == True
             ):
                 self.pos.x = self.pos.x + 1
                 self.movementCooldown = self.defaultCooldown
+                cameraOffset.x += 1
 
 
 player = Player()
@@ -142,15 +194,41 @@ while True:
     for i in range(len(mapa)):
         for j in range(len(mapa[0])):
             if mapa[i][j] == "X":
-                prozor.blit(StoneFloor, (j * 100, i * 100))
+                prozor.blit(
+                    StoneFloor,
+                    (
+                        j * 100 - int(cameraOffset.x) * 100,
+                        i * 100 - int(cameraOffset.y) * 100,
+                    ),
+                )
             if mapa[i][j] == "O":
-                prozor.blit(Dirt, (j * 100, i * 100))
-            if mapa[i][j] == "OS":
-                prozor.blit(Dirt, (j * 100, i * 100))
-                prozor.blit(Chest1, (j * 100, i * 100))
+                prozor.blit(
+                    Dirt,
+                    (
+                        j * 100 - int(cameraOffset.x) * 100,
+                        i * 100 - int(cameraOffset.y) * 100,
+                    ),
+                )
+            if mapa[i][j] == "W":
+                prozor.blit(
+                    water1,
+                    (
+                        j * 100 - int(cameraOffset.x) * 100,
+                        i * 100 - int(cameraOffset.y) * 100,
+                    ),
+                )
+            if mapa[i][j] == "S":
+                prozor.blit(
+                    sand,
+                    (
+                        j * 100 - int(cameraOffset.x) * 100,
+                        i * 100 - int(cameraOffset.y) * 100,
+                    ),
+                )
     for i in range(len(entityList)):
         entityList[i].Update()
         entityList[i].Draw()
     player.Update()
     player.Draw()
     pygame.display.flip()
+    prozor.fill(pygame.Color("blue"))
